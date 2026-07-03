0eb36d442eb3dfd9a8cb0b346393b0a6d7c9f6ae
Jaiveer Singh Kapoor <jaiveersinghkapoor08@gmail.com>
2026-07-03 22:38:57 +0100

    Finished with the project. I'm glad I didn't take any help from AI, learnt a lot from this project. Proud that I finished it.

--- main.py
+++ main.py
@@ -9,18 +9,32 @@ import tkinter as tk
 calculation = ""
 
 def add_to_calculation(symbol):
-    pass
+    global calculation
+    calculation += str(symbol)
+    result_text.delete(1.0, tk.END)
+    result_text.insert(1.0, calculation)
 
 def evaluate_calculation():
-    pass
+    global calculation
+    try:
+        result = str(eval(calculation))
+        result_text.delete(1.0, tk.END)
+        result_text.insert(1.0, result)
+        calculation = result
+    except Exception: #so that it doesn't say "Error" when I keyboard interrupt
+        calculation = "Error"
+        result_text.delete(1.0, tk.END)
+        result_text.insert(1.0, calculation)
+        root.after(500, lambda: result_text.delete(1.0, tk.END)) #34 MIN LONGEST DEBUG BECAUSE I TRIED MAKING IT MYSELF, ALL I HAD TO DO WAS ADD LAMBDA
+        calculation = ""
+        
 
+
+    
 def clear_calculation():
-    pass
-
-
-
-
-
+    global calculation
+    calculation = ""
+    result_text.delete(1.0, tk.END)
 
 root = tk.Tk()
 bg = "#000d24"
@@ -31,14 +45,76 @@ root.title("My First GUI")
 #making the window look so pretty and so beautiful and just looking like a wow
 root.title("W calc")
 root.config(bg=bg) #midnigght blue so tuff
-root.geometry("400x650") #size of the window
+root.geometry("400x553") #size of the window
 root.resizable(False, False) #cant resize the window haha imagine trying to resize this hahaha😂😂😂😂
 
+#all the buttons
 
-label = tk.Label(root, text="yo wassup", bg=bg, fg="white")
-label.pack()
-button = tk.Button(root, text="click me or else ull see", command=lambda: label.config(text="ur such a good boy"), bg=bg, fg="white")
-button.pack()
+
+result_text = tk.Text(root, height=2, width=14, font=("Arial", 35), bg="#000d24", fg="white", bd=0, highlightthickness=0)
+result_text.grid(columnspan=5)
+
+btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_1.grid(row=1, column=0)
+
+btn_2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_2.grid(row=1, column=1)
+
+btn_3 = tk.Button(root, text="3", command=lambda: add_to_calculation(3), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_3.grid(row=1, column=2)
+
+btn_clear = tk.Button(root, text="C", command=clear_calculation, width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_clear.grid(row=1, column=3)
+
+btn_4 = tk.Button(root, text="4", command=lambda: add_to_calculation(4), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_4.grid(row=2, column=0)
+
+btn_5 = tk.Button(root, text="5", command=lambda: add_to_calculation(5), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_5.grid(row=2, column=1)
+
+btn_6 = tk.Button(root, text="6", command=lambda: add_to_calculation(6), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_6.grid(row=2, column=2)
+
+btn_plus = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_plus.grid(row=2, column=3)
+
+btn_7 = tk.Button(root, text="7", command=lambda: add_to_calculation(7), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_7.grid(row=3, column=0)
+
+btn_8 = tk.Button(root, text="8", command=lambda: add_to_calculation(8), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_8.grid(row=3, column=1)
+
+btn_9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_9.grid(row=3, column=2)
+
+btn_minus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_minus.grid(row=3, column=3)
+
+btn_square = tk.Button(root, text="x²", command=lambda: add_to_calculation("**2"), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_square.grid(row=4, column=0)
+
+btn_0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_0.grid(row=4, column=1)
+
+btn_multiply = tk.Button(root, text="*", command=lambda: add_to_calculation("*"), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_multiply.grid(row=4, column=2)
+
+btn_divide = tk.Button(root, text="/", command=lambda: add_to_calculation("/"), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_divide.grid(row=4, column=3)
+
+btn_period = tk.Button(root, text=".", command=lambda: add_to_calculation("."), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_period.grid(row=5, column=2)
+
+btn_equals = tk.Button(root, text="=", command=evaluate_calculation, width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_equals.grid(row=5, column=3)
+
+btn_paranthesis_open = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_paranthesis_open.grid(row=5, column=0)
+
+btn_paranthesis_close = tk.Button(root, text=")", command=lambda: add_to_calculation(")"), width=5, height=2, font=("Arial", 20), bg="#000d24", fg="white", bd=0, highlightthickness=0, borderwidth=2)
+btn_paranthesis_close.grid(row=5, column=1)
+
+
 
 #oooooooooohhh, stayin' alive, stayin' alive
 root.mainloop()
--- mistakes-I-made.md
+++ mistakes-I-made.md
@@ -4,15 +4,15 @@
 
 okay so when I was creating the init-gui func, I ran the script without actually calling the func, and when I added code to call it, i realised my main loop was above most of the initialisation, am i cooked or am i cooked
 
-okay after fixing all that, I decided to use my own neurons (not artificial ones) and put the loop outside the init function after everything.
+after fixing all that, I decided to use my own neurons (not artificial ones) and put the loop outside the init function after everything.
 
 when I took root outside, the definition for root was still inside the func, so I decided to put that at the top before everything else
 
-okay so till now I was following this webpage - https://www.pythonguis.com/tutorials/create-gui-tkinter/
+till now I was following this webpage - https://www.pythonguis.com/tutorials/create-gui-tkinter/
 but now ive decided to go on da big YT and watch this - https://www.youtube.com/watch?v=6aKmTV6eYt8
 maybe it's better
 
-okay in between the video i decided to remove the stupid function because there's no point and it's ruining everything RRAAAGGGHHH
+in between the video i decided to remove the stupid function because there's no point and it's ruining everything RRAAAGGGHHH
 
 it's talking bout sum software called PyUIBuilder so ill install that
 nvm it's paid i aint paying for that
@@ -21,7 +21,42 @@ that video didnt help at all bro what
 
 gonna watch another one - https://www.youtube.com/watch?v=NzSCNjn4_RI
 
-gonna completely overhaul the old code
+now ima completely overhaul the old code
 it's pretty late (around 20:25) so I'm gonna have dinner then chill or sleep, couldn't learn if I wanted to anyway.
 
 
+**03/07/2026**
+
+hmm, maybe a bit too long of a break..?
+
+anyways I'm resuming the video and basically copying most of the stuff except for the stuff that I know I shouldn't copy down, I've tinkered with the sizes a lot and I'm about to start actually making the functions.
+
+okay added everything, and it works! just that when I divide by 0 and get "Error", and then I start calculating again, it adds to the word, so it keeps on repeating the error. I'm gonna make it so that after like a 0.5s delay, the result_text clears again.
+
+hmm, for some reason it delays before it clears everything, and when I start calculating again, it shows "Error". tryna fix that now.
+I tried adding buffers to every step of the function, but it didn't work.. bruh
+
+okay, so maybe the problem is that the root can't refresh before the time.sleep(0.4) function is called. also added calculation="" after printing the calculation and before deleting the entire line.
+
+well, that made progress, although my Error message is gone, it's not added to my new calculation.
+
+so I guessed that tk has it's own sleep function, and it does! it's called after(), and the article specified I shouldn't use time.sleep as I'll have issues (which I do)
+
+and WOW it has the same problem?? whyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+
+NO. WAY.
+IT'S ALL BECAUSE OF LAMBDA BRO LAMBDA LAMBDA LAMBDA
+IDK IF I SHOULD BE HAPPY WITH OR ANGRY AT IT
+RRAAAAAGGGGGGGHHHHHHHHHHH
+I USED IT IN MY BUTTONS TOO HOW DID I FORGET
+
+anyways
+
+made a few more changes, like the previous result now carries on in your calculations unless u hit clear
+division looks a little ugly, but I can't be bothered rn to change that. 
+
+I consider this project finished. 
+
+Good night.
+
+**Finished 22:36 03/07/26**
